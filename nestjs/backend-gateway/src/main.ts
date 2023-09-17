0738f7e9bbda54b0f77e2578b65f3e03bb764e29
JoaoAlves92 <jvamarangonia@gmail.com>
2023-09-17 02:24:18 -0300

    deploy nest

--- nestjs/backend-gateway/src/main.ts
+++ nestjs/backend-gateway/src/main.ts
@@ -1,18 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { Kafka } from 'kafkajs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  const kafka = new Kafka({
+    clientId: process.env.KAFKA_CLIENT_ID,
+    brokers: [process.env.KAFKA_HOST],
+    ssl: process.env.KAFKA_USE_SSL === 'true',
+    sasl: {
+      mechanism: 'plain',
+      username: process.env.KAFKA_SASL_USERNAME,
+      password: process.env.KAFKA_SASL_PASSWORD,
+    },
+  });
+
   app.connectMicroservice({
     transport: Transport.KAFKA,
     options: {
-      client: {
-        clientId: process.env.KAFKA_CLIENT_ID,
-        brokers: [process.env.KAFKA_HOST],
-        ssl: process.env.KAFKA_USE_SSL === 'true',
-      },
+      client: kafka,
       consumer: {
         groupId: process.env.KAFKA_CONSUMER_GROUP_ID,
       },
